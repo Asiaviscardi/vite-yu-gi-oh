b85883b1bb753803a7675f14ff92f405c7f4283d
Asiaviscardi <asia.viscardii@gmail.com>
2023-10-12 14:03:53 +0200

    inserimento header e main

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    name:'Header'
+
+}
+</script>
+
+<template>
+
+    <h1>header</h1>
+  
+</template>
+
+<style>
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    name:'Main'
+
+}
+</script>
+
+<template>
+
+    <h1>main</h1>
+  
+</template>
+
+<style>
+
+</style>
